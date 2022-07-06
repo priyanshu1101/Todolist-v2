c142de42b6972b94ff28c7e7cc7de9f01b08edb2
Priyanshu <priyanshubansal35@gmail.com>
2022-07-06 21:24:05 +0530

    After applying css to the list

--- app.js
+++ app.js
@@ -4,22 +4,39 @@ const app=express();
 const port=3000;
 
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended : true}))
+app.use(express.static("public"));
 
 
 const options = { day: 'numeric', month: 'long', weekday: 'long', year: 'numeric'};
 var date=new Date();
 var items=[];
+var workitems=[];
 
 app.get('/',function(req,res)
 {
-    res.render('list',{day:date.toLocaleDateString('en-us',options),itemlist:items});
+    res.render('list',{list_heading:date.toLocaleDateString('en-us',options),itemlist:items});
 })
 
 app.post('/',function(req,res)
 {
-    items.push(req.body.item);
-    res.render('list',{day:date.toLocaleDateString('en-us',options),itemlist:items});
+    if(req.body.list=="Work")
+    {
+        if(req.body.item.trim()!="")
+        workitems.push(req.body.item.trim());
+        res.redirect("/work");
+    }
+    else{
+        if(req.body.item.trim()!="")
+        items.push(req.body.item.trim());
+        res.redirect("/");
+    }
+})
+
+app.get('/work',function(req,res)
+{
+    res.render('list',{list_heading:"Work",itemlist:workitems});
 })
 
 app.listen(port) 
